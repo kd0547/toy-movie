e7cd095d4d3a958bb20c51087826122f051dfc42
kdw <dawon011@naver.com>
2022-05-18 21:44:11 +0900

    test

---
+++ movieView/src/main/java/com/movie/MovieViewApplication.java
@@ -0,0 +1,13 @@
+package com.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieViewApplication.class, args);
+	}
+
+}
